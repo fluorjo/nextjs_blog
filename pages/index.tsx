4b46d8ab5ecc5d7cd5c9cac70d962c6c7c85a311
fluorjo <fluorjo@naver.com>
2023-12-24 02:46:46 +0900

    get staticProps. post list

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,19 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import homeStyles from "@/styles/Home.module.css";
+import { getSortedPostsData } from "@/lib/posts";
 
-const Home: NextPage = () => {
+const Home = ({
+  allPostsData,
+}: {
+  allPostsData: {
+    date: string;
+    title: string;
+    id: string;
+  }[];
+}) => {
   return (
     <div>
       <Head>
@@ -15,11 +24,28 @@ const Home: NextPage = () => {
         <p>website~!!!~</p>
       </section>
       <section className={`${homeStyles.headingMd} ${homeStyles.padding1px}`}>
-        <h2 className={homeStyles.headingLg}>blog</h2>
-        <ul className={homeStyles.list}></ul>
+        <h2 className={homeStyles.headingLg}>Blog</h2>
+        <ul className={homeStyles.list}>
+          {allPostsData.map(({ id, title, date }) => (
+            <li className={homeStyles.listItem} key={id}>
+              <a>{title}</a>
+              <br />
+              <small className={homeStyles.lightText}>{date}</small>
+            </li>
+          ))}
+        </ul>
       </section>
     </div>
   );
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
