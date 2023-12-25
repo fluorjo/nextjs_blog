830f6e0719cd433070f146dca4ebb67e808fe108
fluorjo <fluorjo@naver.com>
2023-12-25 19:44:34 +0900

    feat:add postitem, install prettier, eslint

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,5 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
-  return (
-    <div>
-      home~
-    </div>
-  )
+  return <div>home~</div>
 }
